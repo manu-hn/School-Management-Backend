856f3628d6e566fb29a8953bb7b7ceff7233da2d
manu-hn <manuu.hn11@gmail.com>
2024-07-26 12:20:25 +0530

    "Implemented Database Schema Models for admin, teacher, student, and subject"

---
+++ src/models/studentScheme.model.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ['male', 'female'],
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    },
+    salary: {
+        type: Number,
+        required: true,
+    },
+    rollNum: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    sclassName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'sclass',
+        required: true,
+    },
+    school: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'admin',
+        required: true,
+    },
+    role: {
+        type: String,
+        default: "Student"
+    },
+    isFeesPaid: {
+        type: Boolean,
+        default: false,
+    },
+    examResult: [
+        {
+            subName: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'subject',
+            },
+            marksObtained: {
+                type: Number,
+                default: 0
+            }
+        }
+    ],
+    attendance: [{
+        date: {
+            type: Date,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ['Present', 'Absent'],
+            required: true
+        },
+        subName: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'subject',
+            required: true
+        }
+    }]
+});
+
+module.exports = mongoose.model("student", studentSchema);
